e1c65969e5110a88e1018488efd219bbb1fdf550
zhengyunhong.zyh <337361684@qq.com>
2022-09-05 12:29:23 +0800

    shell file modify

--- tools/common/env.sh
+++ tools/common/env.sh
@@ -2,7 +2,7 @@
 
 export INSTALL_PATH="/home/hadoop/flink-sql-benchmark"
 
-export FLINK_HOME=${INSTALL_PATH}/packages/flink-1.14.3
+export FLINK_HOME=${INSTALL_PATH}/packages/flink-1.15.3
 export FLINK_TEST_JAR=${INSTALL_PATH}/flink-tpcds/target/flink-tpcds-0.1-SNAPSHOT-jar-with-dependencies.jar
 
 export SPARK_HOME=${INSTALL_PATH}/packages/spark-3.2.1
@@ -13,6 +13,9 @@ export SPARK_BEELINE_SERVER="jdbc:hive2://emr-header-1:10001"
 export SCALE=10000
 export FLINK_TEST_DB=tpcds_bin_orc_$SCALE
 export SPARK_TEST_DB=spark_tpcds_bin_orc_$SCALE
+
+# export FLINK_TEST_DB=tpcds_bin_partitioned_orc_$SCALE
+# export SPARK_TEST_DB=spark_tpcds_bin_partitioned_orc_$SCALE
 
 
 
